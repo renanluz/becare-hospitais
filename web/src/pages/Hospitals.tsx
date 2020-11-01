1ea71b64a76d112f0199c52557980cba5a27133f
Renanluz <ren4n@live.com>
2020-11-01 16:02:45 -0300

    Create hospital page

---
+++ web/src/pages/Hospitals.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi'
+
+import mapMarkerImg from '../assets/images/map-marker.svg';
+
+import '../styles/pages/hospitals.css'
+
+function Hospitals() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="BeCare"/>
+                    
+                    <h2>Escolha um hospital no mapa</h2>
+                    <p></p>
+                </header>
+
+                <footer>
+                    <strong>São José do Rio Preto</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-hospital">
+                <FiPlus size={32} color="#FFF"/>
+            </Link>    
+
+        </div>
+    );
+}
+
+export default Hospitals;
