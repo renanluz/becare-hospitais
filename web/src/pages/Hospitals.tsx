ce6fcd6687688365cfd6c0999c8c9bfcb851e8b7
Renanluz <ren4n@live.com>
2020-11-01 19:18:01 -0300

    Add map

--- web/src/pages/Hospitals.tsx
+++ web/src/pages/Hospitals.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi'
 
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../assets/images/map-marker.svg';
 
 import '../styles/pages/hospitals.css'
@@ -23,7 +27,15 @@ function Hospitals() {
                 </footer>
             </aside>
 
-            <div></div>
+            <Map
+                center={[20.7959755, -49.3353081]}
+                zoom={14}
+                style={{ width: '100%', height: '100%' }}
+            >
+                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+                <TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+            </Map>
 
             <Link to="" className="create-hospital">
                 <FiPlus size={32} color="#FFF"/>
