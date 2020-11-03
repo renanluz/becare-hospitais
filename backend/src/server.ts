39a5bd7822754cf4b35215f990ff192df3419e2b
Renanluz <ren4n@live.com>
2020-11-02 22:03:04 -0300

    Add dependency cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
+import 'express-async-errors';
 import mongoose from 'mongoose';
+
 import routes from './routes'
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -12,9 +17,11 @@ mongoose.connect('mongodb+srv://becare:becare@cluster0.qb0pn.mongodb.net/becare?
 }).then(() => console.log("MongoDB conectado"))
 .catch((err) => console.error("Erro ao conectar com o MongoDB" + err));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(express.urlencoded({ extended: true }));
+app.use(errorHandler);
 
 app.listen(3333, () => {
     console.log("Servidor rodando em http://127.0.0.1:3333");
