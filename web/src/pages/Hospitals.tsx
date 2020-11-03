62c785a60ae7476e53a9227b88ed7fdd619fabc4
Renanluz <ren4n@live.com>
2020-11-03 01:13:36 -0300

    Fix bugs maps

--- web/src/pages/Hospitals.tsx
+++ web/src/pages/Hospitals.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi'
 
-import { Map, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -27,15 +27,15 @@ function Hospitals() {
                 </footer>
             </aside>
 
-            <Map
+            <MapContainer
                 center={[20.7959755, -49.3353081]}
-                zoom={14}
+                zoom={13}
                 style={{ width: '100%', height: '100%' }}
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
                 <TileLayer 
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
-            </Map>
+            </MapContainer>
 
             <Link to="" className="create-hospital">
                 <FiPlus size={32} color="#FFF"/>
